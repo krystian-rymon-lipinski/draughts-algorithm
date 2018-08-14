851f0ac5f27343db83aad2e5096cbc364d08528f
Krystian <rymon9696@gmail.com>
2018-08-14 15:39:30 +0200

    V. 0.03
    Brown tiles with id's and listeners. Pawns drawn.

--- checkers/app/src/main/java/com/krystian/checkers/GameActivity.java
+++ checkers/app/src/main/java/com/krystian/checkers/GameActivity.java
@@ -6,32 +6,34 @@ import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import java.util.ArrayList;
-
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public final static int NUMBER_OF_PAWNS = 20;
+    public final static int NUMBER_OF_PAWNS = 20; //both white and brown
+    public final static int NUMBER_OF_TILES = 100;
+    public final static int NUMBER_OF_PLAYABLE_TILES = 50;
 
     GridLayout board;
-    int counter = 0;
-
+    View[] tile = new View[NUMBER_OF_TILES];
+    View[] playableTile = new View[NUMBER_OF_PLAYABLE_TILES];
+    ArrayList<Pawn> whitePawn = new ArrayList<>();
+    ArrayList<Pawn> brownPawn = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
         board = (GridLayout) findViewById(R.id.board);
-        board.setOnClickListener(this);
         measureBoard();
-
     }
 
     public void onClick(View view) {
         switch(view.getId()) {
-            case R.id.board:
+            case 0: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11: case 12:
+            case 1:
+            case 2:
+                view.getBackground().setAlpha(70);
                 break;
         }
     }
@@ -41,29 +43,48 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 int width = board.getWidth();
                 int height = board.getHeight();
-                Log.v("Wysokość: ", "" + height);
-                Log.v("Szerokość: ", "" + width);
-                drawBoard(width, height);
+                drawBoard(width, height);  //to calculate width and height of a single tile
             }
         });
     }
 
     public void drawBoard(int width, int height) {
-        View[] tile = new View[100];
+        int brownTileCounter = 0; //to set value for brown tiles which will be needed for game mechanics (for example possible moves)
         for(int i=0; i < tile.length; i++) {
-            tile[i] = new TextView(this);
-            tile[i].setLayoutParams(new LinearLayout.LayoutParams(width/10, height/10));
-            if( (i%2 == 0 && (i/10)%2 == 0) || (i%2 != 0 && (i/10)%2 != 0) )
+            tile[i] = new View(this);
+            tile[i].setLayoutParams(new LinearLayout.LayoutParams(width/10, height/10)); //10 tiles x 10 tiles board
+            if( (i%2 == 0 && (i/10)%2 == 0) || (i%2 != 0 && (i/10)%2 != 0) )  //which tiles should be white
                 tile[i].setBackgroundColor(getResources().getColor(R.color.whiteTile));
-            else
-                tile[i].setBackgroundColor(getResources().getColor(R.color.brownTile));
+            else {
+                playableTile[brownTileCounter] = tile[i];
+                playableTile[brownTileCounter].setId(brownTileCounter);
+                playableTile[brownTileCounter].setOnClickListener(this);
+                brownTileCounter++;
+            }
 
             board.addView(tile[i]);
         }
 
-        ArrayList<Pawn> whitePawns = new ArrayList<>();
         for(int i=0; i<NUMBER_OF_PAWNS; i++) {
-            whitePawns.add(new Pawn( (i*2)+1, false, true ));
+            whitePawn.add(new Pawn( NUMBER_OF_PLAYABLE_TILES-i, true, false));
+            brownPawn.add(new Pawn( i+1, false, false));
+        }
+        drawPawns();
+    }
+
+    public void drawPawns() { //will be useful after every move
+        for(int i=0; i<NUMBER_OF_PLAYABLE_TILES; i++) {
+            for (int j = 0; j < whitePawn.size(); j++) {
+                if (whitePawn.get(j).getPosition()-1 == i) {
+                    playableTile[i].setBackgroundResource(R.drawable.white_pawn);
+                }
+            }
+
+            for(int j=0; j<brownPawn.size(); j++) {
+                if (brownPawn.get(j).getPosition()-1 == i) {
+                    playableTile[i].setBackgroundResource(R.drawable.brown_pawn);
+                }
+            }
         }
     }
 }
--- checkers/app/src/main/java/com/krystian/checkers/Pawn.java
+++ checkers/app/src/main/java/com/krystian/checkers/Pawn.java
@@ -2,20 +2,21 @@ package com.krystian.checkers;
 
 public class Pawn {
     private int position;
-    private boolean isQueen;
     private boolean isWhite;
+    private boolean isQueen;
 
-    public Pawn(int position, boolean isQueen, boolean isWhite) {
+    public Pawn(int position, boolean isWhite, boolean isQueen) {
         this.position = position;
-        this.isQueen = isQueen;
         this.isWhite = isWhite;
+        this.isQueen = isQueen;
     }
 
     public int getPosition() { return position; }
-    public boolean getIsQueen() { return isQueen; }
     public boolean getIsWhite() { return isWhite; }
+    public boolean getIsQueen() { return isQueen; }
 
     public void setPosition(int position) { this.position = position; }
-    public void setIsQueen(boolean isQueen) { this.isQueen = isQueen; }
     public void setIsWhite(boolean isWhite) { this.isWhite = isWhite; }
+    public void setIsQueen(boolean isQueen) { this.isQueen = isQueen; }
+
 }
